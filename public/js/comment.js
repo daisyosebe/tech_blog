1572f19eb229e5992b40cc4f89b9ccedae3c6c37
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-06-01 19:14:58 -0500

    comment js

---
+++ public/js/comment.js
@@ -0,0 +1,27 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const comment_text = document.querySelector('#comment-text').value.trim();
+    const post_id = window.location.pathname.split('/')[2];
+  
+    if (comment_text) {
+      const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({ comment_text, post_id }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.reload();
+      } else {
+        alert('Failed to add comment');
+      }
+    }
+  };
+  
+  document
+    .querySelector('#comment-form')
+    .addEventListener('submit', commentFormHandler);
+  
